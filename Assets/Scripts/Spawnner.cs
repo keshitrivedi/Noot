2b23787bc0facdc7fd6699488df51a8b8f4e9165
Cashtrii <167008920+keshitrivedi@users.noreply.github.com>
2026-04-21 02:23:13 +0530

    outline and spawhn

--- Assets/Scripts/Spawnner.cs
+++ Assets/Scripts/Spawnner.cs
@@ -1,16 +1,35 @@
+using System.Collections;
 using UnityEngine;
 
 public class Spawnner : MonoBehaviour
 {
+    private float xLimUp;
+    private float xLimLow;
+    private float zLimUp;
+    private float zLimLow;
+
+    public GameObject baccha;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        xLimUp = transform.position.x + 20;
+        xLimLow = transform.position.x - 20;
+
+        zLimUp = transform.position.z + 20;
+        zLimLow = transform.position.z - 20;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Spawn(Random.Range(200, 300));
+    }
+
+    IEnumerator Spawn(float delaySeconds)
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        Vector3 randomSpawnPos = new Vector3(Random.Range(xLimLow, xLimUp), 0, Random.Range(zLimLow, zLimUp));
+
+        Debug.Log(randomSpawnPos);
     }
 }
